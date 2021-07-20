74ce99f8f5513ae69ad3b923b2d3f8a81074f099
Fatima <fcamero12@gmail.com>
2021-07-19 21:33:52 -0700

    Add and refactor views; add request tests

--- backend/energyapi/urls.py
+++ backend/energyapi/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from energyapi import views
 
+
 urlpatterns = [
     path('energydata', views.EnergyDataList.as_view()),
+    path('energydata/<int:pk>', views.EnergyDetail.as_view())
 ]
--- backend/energyapi/views.py
+++ backend/energyapi/views.py
@@ -1,11 +1,25 @@
-from django.http import Http404
+from django.http import Http404, JsonResponse
+from django.http.response import HttpResponse
+from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework import generics
 from rest_framework.response import Response
 from energyapi.models import EnergyData
 from energyapi.serializers import EnergyDataSerializer
 
+# This is the model to keep, will be adding list view for each region
+# class EnergyDataList(generics.ListAPIView):
+#     """
+#     List all energy data
+#     """
+#     def get(self, request, format=None):
+#         energy_data = EnergyData.objects.all()
+#         serializer = EnergyDataSerializer(energy_data, many=True)
+#         return Response(serializer.data)
 
+
+## The following view is what is currently working with frontend
 class EnergyDataList(generics.ListAPIView):
     """
     List all energy data
@@ -17,3 +31,19 @@ class EnergyDataList(generics.ListAPIView):
         if region is not None:
             queryset = queryset.filter(region=region)
         return queryset
+
+
+class EnergyDetail(generics.RetrieveAPIView):
+    """
+    Retrieve a single energy data record
+    """
+    # def get_object(self, pk):
+    #     try:
+    #         return EnergyData.objects.get(pk=pk)
+    #     except EnergyData.DoesNotExist:
+    #         raise Http404
+    queryset = EnergyData.objects.all()
+    serializer_class = EnergyDataSerializer
+        
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
