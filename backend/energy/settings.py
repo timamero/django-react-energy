e6426ffce43ad82c6b4a97a8d96ed887ca3c507e
Fatima <fcamero12@gmail.com>
2021-07-21 14:38:36 -0700

    Fix

--- backend/energy/settings.py
+++ backend/energy/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3u^j)pi+lu&a#r19_3z#3l%bi(((cx-n9m=-&z=+8^h$73x%l5'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
